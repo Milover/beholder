b3787b4433670b56b1c3050b16b84d4640655e17
Philipp Milovic <milovic.ph@gmail.com>
2024-05-11 22:49:21 +0200

    Add cropping and rotation to Go code

--- internal/ocr/processing_ops.h
+++ internal/ocr/processing_ops.h
@@ -7,6 +7,7 @@ extern "C" {
 #endif
 
 #ifdef __cplusplus
+typedef ocr::Crop* Crp;
 typedef ocr::DrawTextBoxes* DrawTB;
 typedef ocr::GaussianBlur* GaussBlur;
 typedef ocr::Invert* Inv;
@@ -14,24 +15,29 @@ typedef ocr::MedianBlur* MedBlur;
 typedef ocr::Morphology* Morph;
 typedef ocr::NormalizeBrightnessContrast* NormBC;
 typedef ocr::Resize* Rsz;
+typedef ocr::Rotate* Rot;
 typedef ocr::Threshold* Thresh;
 #else
+typedef void* Crp;
 typedef void* DrawTB;
 typedef void* GaussBlur;
 typedef void* Inv;
 typedef void* MedBlur;
 typedef void* Morph;
 typedef void* NormBC;
+typedef void* Rot;
 typedef void* Rsz;
 typedef void* Thresh;
 #endif
 
+Crp Crp_New(int left, int right, int top, int bottom);
 DrawTB DrawTB_New(float* color, int thickness);
 GaussBlur GaussBlur_New(int kW, int kH, float sX, float sY);
 Inv Inv_New();
 MedBlur MedBlur_New(int ksize);
 Morph Morph_New(int kTyp, int kW, int kH, int typ, int iter);
 NormBC NormBC_New(float clipPct);
+Rot Rot_New(float angle);
 Rsz Rsz_New(int width, int height);
 Thresh Thresh_New(float val, float maxVal, int typ);
 
