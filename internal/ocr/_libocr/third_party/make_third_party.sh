ec89c946614c5352278b2e20e0582b7e2718cb60
Philipp Milovic <milovic.ph@gmail.com>
2024-05-30 09:50:24 +0200

    Use a locally built static Leptonica library

--- internal/ocr/_libocr/third_party/make_third_party.sh
+++ internal/ocr/_libocr/third_party/make_third_party.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 OPENCV_VERSION="4.9.0"
+LEPTONICA_VERSION="1.84.1"
 
+#
 # Build OpenCV
+#
 
 git clone --depth=1 --branch "$OPENCV_VERSION" https://github.com/opencv/opencv.git
 cd opencv
@@ -14,3 +17,30 @@ cmake -DCMAKE_BUILD_TYPE=Release \
 	  ../
 make -j4
 make install
+
+#
+# Build Leptonica
+#
+
+git clone --depth=1 --branch "$LEPTONICA_VERSION" https://github.com/DanBloomberg/leptonica.git
+cd leptonica
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release \
+	  -DCMAKE_INSTALL_PREFIX=../../../share \
+	  -DBUILD_SHARED_LIBS=OFF \
+	  -DPNG_LIBRARY=../../../share/lib/opencv4/3rdparty/liblibpng.a \
+	  -DZLIB_LIBRARY=../../../share/lib/opencv4/3rdparty/libzlib.a \
+	  -DTIFF_LIBRARY=../../../share/lib/opencv4/3rdparty/liblibtiff.a \
+	  -DJPEG_LIBRARY=../../../share/lib/opencv4/3rdparty/liblibjpeg-turbo.a \
+	  -DOPENJPEG_LIBRARIES=../../../share/lib/opencv4/3rdparty/liblibopenjp2.a \
+	  -DENABLE_GIF=OFF \
+	  -DENABLE_WEBP=ON \
+	  -DWebP_LIBRARIES=../../../share/lib/opencv4/3rdparty/liblibwebp.a \
+	  ../
+make -j4
+make install
+
+#
+# Build tesseract
+#
