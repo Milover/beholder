554661f2fce5b7e4f135afe7a8b0d0708c22049c
Philipp Milovic <milovic.ph@gmail.com>
2024-05-31 22:10:35 +0200

    Use statically built C++ libraries

--- internal/ocr/_libocr/third_party/make_third_party.sh
+++ internal/ocr/_libocr/third_party/make_third_party.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
+# run from this directory
+cd ${0%/*} || exit 1
+
+# exit on error
+set -e
+
+#------------------------------------------------------------------------------
+
 OPENCV_VERSION="4.9.0"
 LEPTONICA_VERSION="1.84.1"
+TESSERACT_VERSION="5.3.4"
 
-#
+THIRD_PARTY_DIR="$PWD"
+
+#------------------------------------------------------------------------------
 # Build OpenCV
-#
 
 git clone --depth=1 --branch "$OPENCV_VERSION" https://github.com/opencv/opencv.git
 cd opencv
@@ -13,34 +23,63 @@ mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release \
 	  -DBUILD_SHARED_LIBS=OFF \
-	  -DCMAKE_INSTALL_PREFIX=../../../share \
+	  -DCMAKE_INSTALL_PREFIX="$THIRD_PARTY_DIR/../share" \
 	  ../
 make -j4
 make install
+cd "$THIRD_PARTY_DIR"
 
-#
+#------------------------------------------------------------------------------
 # Build Leptonica
-#
 
 git clone --depth=1 --branch "$LEPTONICA_VERSION" https://github.com/DanBloomberg/leptonica.git
 cd leptonica
+git apply "../patches/leptonica.patch"
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release \
-	  -DCMAKE_INSTALL_PREFIX=../../../share \
+	  -DCMAKE_INSTALL_PREFIX="$THIRD_PARTY_DIR/../share" \
 	  -DBUILD_SHARED_LIBS=OFF \
-	  -DPNG_LIBRARY=../../../share/lib/opencv4/3rdparty/liblibpng.a \
-	  -DZLIB_LIBRARY=../../../share/lib/opencv4/3rdparty/libzlib.a \
-	  -DTIFF_LIBRARY=../../../share/lib/opencv4/3rdparty/liblibtiff.a \
-	  -DJPEG_LIBRARY=../../../share/lib/opencv4/3rdparty/liblibjpeg-turbo.a \
-	  -DOPENJPEG_LIBRARIES=../../../share/lib/opencv4/3rdparty/liblibopenjp2.a \
+	  -DPNG_LIBRARY="$THIRD_PARTY_DIR/../share/lib/opencv4/3rdparty/liblibpng.a" \
+	  -DPNG_PNG_INCLUDE_DIR="$THIRD_PARTY_DIR/opencv/3rdparty/libpng" \
+	  -DZLIB_LIBRARY="$THIRD_PARTY_DIR/../share/lib/opencv4/3rdparty/libzlib.a" \
+	  -DZLIB_INCLUDE_DIR="$THIRD_PARTY_DIR/opencv/3rdparty/zlib" \
+	  -DTIFF_LIBRARY="$THIRD_PARTY_DIR/../share/lib/opencv4/3rdparty/liblibtiff.a" \
+	  -DTIFF_INCLUDE_DIR="$THIRD_PARTY_DIR/opencv/3rdparty/libtiff;$THIRD_PARTY_DIR/opencv/build/3rdparty/libtiff" \
+	  -DJPEG_LIBRARY:FILEPATH="$THIRD_PARTY_DIR/../share/lib/opencv4/3rdparty/liblibjpeg-turbo.a" \
+	  -DJPEG_INCLUDE_DIR:PATH="$THIRD_PARTY_DIR/opencv/3rdparty/libjpeg-turbo/src;$THIRD_PARTY_DIR/opencv/build/3rdparty/libjpeg-turbo" \
+	  -DWebP_LIBRARY="$THIRD_PARTY_DIR/../share/lib/opencv4/3rdparty/liblibwebp.a" \
+	  -DWebP_INCLUDE_DIR="$THIRD_PARTY_DIR/opencv/3rdparty/libwebp/src" \
+	  -DWebP_MUX_LIBRARY="$THIRD_PARTY_DIR/../share/lib/opencv4/3rdparty/liblibwebp.a" \
+	  -DWebP_MUX_INCLUDE_DIR="$THIRD_PARTY_DIR/opencv/3rdparty/libwebp/src" \
+	  -DOPENJPEG_LIBRARIES:FILEPATH="$THIRD_PARTY_DIR/../share/lib/opencv4/3rdparty/liblibopenjp2.a" \
+	  -DOPENJPEG_INCLUDE_DIRS:PATH="$THIRD_PARTY_DIR/opencv/3rdparty/openjpeg/openjp2;$THIRD_PARTY_DIR/opencv/build/3rdparty/openjpeg/openjp2" \
 	  -DENABLE_GIF=OFF \
-	  -DENABLE_WEBP=ON \
-	  -DWebP_LIBRARIES=../../../share/lib/opencv4/3rdparty/liblibwebp.a \
 	  ../
 make -j4
 make install
+cd "$THIRD_PARTY_DIR"
 
-#
+#------------------------------------------------------------------------------
 # Build tesseract
-#
+
+git clone --depth=1 --branch "$TESSERACT_VERSION" https://github.com/tesseract-ocr/tesseract.git
+cd tesseract
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release \
+	  -DCMAKE_INSTALL_PREFIX="$THIRD_PARTY_DIR/../share" \
+	  -DBUILD_SHARED_LIBS=OFF \
+	  -DINSTALL_CONFIGS=OFF \
+	  -DBUILD_TRAINING_TOOLS=OFF \
+	  -DDISABLE_CURL=ON \
+	  -DDISABLE_ARCHIVE=ON \
+	  -DTIFF_INCLUDE_DIR="$THIRD_PARTY_DIR/opencv/3rdparty/libtiff;$THIRD_PARTY_DIR/opencv/build/3rdparty/libtiff;$THIRD_PARTY_DIR/../share/lib/opencv4/3rdparty" \
+	  -DTIFF_LIBRARY="$THIRD_PARTY_DIR/../share/lib/opencv4/liblibtiff.a" \
+	  -DLeptonica_DIR="$THIRD_PARTY_DIR/../share/lib/cmake/leptonica" \
+	  ../
+make -j4
+make install
+cd "$THIRD_PARTY_DIR"
+
+#------------------------------------------------------------------------------
