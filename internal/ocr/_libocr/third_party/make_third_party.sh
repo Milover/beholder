f847cacb83d04b4f4159a097d614a3f6d20b82a3
Philipp Milovic <milovic.ph@gmail.com>
2024-06-27 14:36:03 +0200

    Prettify libocr third party build script

--- internal/ocr/_libocr/third_party/make_third_party.sh
+++ internal/ocr/_libocr/third_party/make_third_party.sh
@@ -13,80 +13,96 @@ LEPTONICA_VERSION="1.84.1"
 TESSERACT_VERSION="5.3.4"
 
 THIRD_PARTY_DIR="$PWD"
+DESTINATION_DIR="$PWD/../share"
 
 #------------------------------------------------------------------------------
 # Build OpenCV
 
 if [ ! -d opencv ]; then
+	echo "setting up OpenCV"
+
 	git clone --depth=1 --branch "$OPENCV_VERSION" https://github.com/opencv/opencv.git
 	cd opencv
 	mkdir build
 	cd build
 	cmake -DCMAKE_BUILD_TYPE=Release \
 		  -DBUILD_SHARED_LIBS=OFF \
-		  -DCMAKE_INSTALL_PREFIX="$THIRD_PARTY_DIR/../share" \
+		  -DCMAKE_INSTALL_PREFIX="$DESTINATION_DIR" \
 		  -DOPENCV_FORCE_3RDPARTY_BUILD=ON \
 		  ../
 	make -j4
 	make install
+
 	cd "$THIRD_PARTY_DIR"
+else
+	echo "OpenCV already set up --- skipping"
 fi
 
 #------------------------------------------------------------------------------
 # Build Leptonica
 
 if [ ! -d leptonica ]; then
+	echo "setting up leptonica"
+
 	git clone --depth=1 --branch "$LEPTONICA_VERSION" https://github.com/DanBloomberg/leptonica.git
 	cd leptonica
 	git apply "../patches/leptonica.patch"
 	mkdir build
 	cd build
 	cmake -DCMAKE_BUILD_TYPE=Release \
-		  -DCMAKE_INSTALL_PREFIX="$THIRD_PARTY_DIR/../share" \
+		  -DCMAKE_INSTALL_PREFIX="$DESTINATION_DIR" \
 		  -DBUILD_SHARED_LIBS=OFF \
-		  -DPNG_LIBRARY="$THIRD_PARTY_DIR/../share/lib/opencv4/3rdparty/liblibpng.a" \
+		  -DPNG_LIBRARY="$DESTINATION_DIR/lib/opencv4/3rdparty/liblibpng.a" \
 		  -DPNG_PNG_INCLUDE_DIR="$THIRD_PARTY_DIR/opencv/3rdparty/libpng" \
-		  -DZLIB_LIBRARY="$THIRD_PARTY_DIR/../share/lib/opencv4/3rdparty/libzlib.a" \
+		  -DZLIB_LIBRARY="$DESTINATION_DIR/lib/opencv4/3rdparty/libzlib.a" \
 		  -DZLIB_INCLUDE_DIR="$THIRD_PARTY_DIR/opencv/3rdparty/zlib" \
-		  -DTIFF_LIBRARY="$THIRD_PARTY_DIR/../share/lib/opencv4/3rdparty/liblibtiff.a" \
+		  -DTIFF_LIBRARY="$DESTINATION_DIR/lib/opencv4/3rdparty/liblibtiff.a" \
 		  -DTIFF_INCLUDE_DIR:PATH="$THIRD_PARTY_DIR/opencv/3rdparty/libtiff;$THIRD_PARTY_DIR/opencv/build/3rdparty/libtiff" \
-		  -DJPEG_LIBRARY:FILEPATH="$THIRD_PARTY_DIR/../share/lib/opencv4/3rdparty/liblibjpeg-turbo.a" \
+		  -DJPEG_LIBRARY:FILEPATH="$DESTINATION_DIR/lib/opencv4/3rdparty/liblibjpeg-turbo.a" \
 		  -DJPEG_INCLUDE_DIR:PATH="$THIRD_PARTY_DIR/opencv/3rdparty/libjpeg-turbo/src;$THIRD_PARTY_DIR/opencv/build/3rdparty/libjpeg-turbo" \
-		  -DWebP_LIBRARY="$THIRD_PARTY_DIR/../share/lib/opencv4/3rdparty/liblibwebp.a" \
+		  -DWebP_LIBRARY="$DESTINATION_DIR/lib/opencv4/3rdparty/liblibwebp.a" \
 		  -DWebP_INCLUDE_DIR="$THIRD_PARTY_DIR/opencv/3rdparty/libwebp/src" \
-		  -DWebP_MUX_LIBRARY="$THIRD_PARTY_DIR/../share/lib/opencv4/3rdparty/liblibwebp.a" \
+		  -DWebP_MUX_LIBRARY="$DESTINATION_DIR/lib/opencv4/3rdparty/liblibwebp.a" \
 		  -DWebP_MUX_INCLUDE_DIR="$THIRD_PARTY_DIR/opencv/3rdparty/libwebp/src" \
-		  -DOPENJPEG_LIBRARIES:FILEPATH="$THIRD_PARTY_DIR/../share/lib/opencv4/3rdparty/liblibopenjp2.a" \
+		  -DOPENJPEG_LIBRARIES:FILEPATH="$DESTINATION_DIR/lib/opencv4/3rdparty/liblibopenjp2.a" \
 		  -DOPENJPEG_INCLUDE_DIRS:PATH="$THIRD_PARTY_DIR/opencv/3rdparty/openjpeg/openjp2;$THIRD_PARTY_DIR/opencv/build/3rdparty/openjpeg/openjp2" \
 		  -DENABLE_GIF=OFF \
 		  ../
 	make -j4
 	make install
+
 	cd "$THIRD_PARTY_DIR"
+else
+	echo "leptonica already set up --- skipping"
 fi
 
 #------------------------------------------------------------------------------
 # Build tesseract
 
 if [ ! -d tesseract ]; then
+	echo "setting up tesseract"
+
 	git clone --depth=1 --branch "$TESSERACT_VERSION" https://github.com/tesseract-ocr/tesseract.git
 	cd tesseract
 	mkdir build
 	cd build
 	cmake -DCMAKE_BUILD_TYPE=Release \
-		  -DCMAKE_INSTALL_PREFIX="$THIRD_PARTY_DIR/../share" \
+		  -DCMAKE_INSTALL_PREFIX="$DESTINATION_DIR" \
 		  -DBUILD_SHARED_LIBS=OFF \
 		  -DINSTALL_CONFIGS=OFF \
 		  -DBUILD_TRAINING_TOOLS=OFF \
 		  -DDISABLE_CURL=ON \
 		  -DDISABLE_ARCHIVE=ON \
-		  -DTIFF_INCLUDE_DIR="$THIRD_PARTY_DIR/opencv/3rdparty/libtiff;$THIRD_PARTY_DIR/opencv/build/3rdparty/libtiff;$THIRD_PARTY_DIR/../share/lib/opencv4/3rdparty" \
-		  -DTIFF_LIBRARY="$THIRD_PARTY_DIR/../share/lib/opencv4/liblibtiff.a" \
-		  -DLeptonica_DIR="$THIRD_PARTY_DIR/../share/lib/cmake/leptonica" \
+		  -DTIFF_INCLUDE_DIR="$THIRD_PARTY_DIR/opencv/3rdparty/libtiff;$THIRD_PARTY_DIR/opencv/build/3rdparty/libtiff;$DESTINATION_DIR/lib/opencv4/3rdparty" \
+		  -DTIFF_LIBRARY="$DESTINATION_DIR/lib/opencv4/liblibtiff.a" \
+		  -DLeptonica_DIR="$DESTINATION_DIR/lib/cmake/leptonica" \
 		  ../
 	make -j4
 	make install
+
 	cd "$THIRD_PARTY_DIR"
+else
+	echo "tesseract already set up --- skipping"
 fi
 
 #------------------------------------------------------------------------------
