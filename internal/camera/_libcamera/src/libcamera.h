970feba59da7a34008884b21655c8ff710535360
Philipp Milovic <milovic.ph@gmail.com>
2024-07-22 00:50:52 +0200

    Add library header

---
+++ internal/camera/_libcamera/src/libcamera.h
@@ -0,0 +1,31 @@
+/*---------------------------------------------------------------------------*\
+
+	camera - Copyright (C) 2024 P. Milovic
+
+-------------------------------------------------------------------------------
+License
+	See the LICENSE file for license information.
+
+Description
+	Headers for the library, collected for ease of use.
+
+\*---------------------------------------------------------------------------*/
+
+#ifndef CAMERA_LIBCAMERA_H
+#define CAMERA_LIBCAMERA_H
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+#include "Camera.h"
+#include "DefaultConfigurator.h"
+#include "Exception.h"
+#include "Image.h"
+#include "ParamEntry.h"
+#include "TransportLayer.h"
+//#include "Utility.h"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+#endif
+
+// ************************************************************************* //
