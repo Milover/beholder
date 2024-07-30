4b281f52e93576263617b79bfcfdc4aebe3f38eb
Philipp Milovic <milovic.ph@gmail.com>
2024-07-30 05:50:45 +0200

    Add runtime flags to 'cam' subcommand and update acquisition loop

--- cmd/cam.go
+++ cmd/cam.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math"
 	"os"
+	"path"
+	"reflect"
 	"runtime"
 	"strconv"
 	"time"
@@ -13,6 +16,23 @@ import (
 	"github.com/Milover/beholder/internal/ocr"
 	"github.com/spf13/cobra"
 )
+
+var (
+	// nImages is the needed number of successfully acquired images for the
+	// program to complete.
+	// It is set to 'infinity' by default, i.e. the program will continually
+	// acquire images withouth stopping.
+	nReqAcquired uint64
+)
+
+func init() {
+	camCmd.Flags().Uint64Var(
+		&nReqAcquired,
+		"n-img",
+		math.MaxUint64,
+		"number of images to successfully acquire before exiting",
+	)
+}
 
 var (
 	camCmd = &cobra.Command{
@@ -26,6 +46,79 @@ var (
 	}
 )
 
+// TODO: move this to an appropriate package.
+// Filename is the runtime configuration for generating file names when
+// writing images to disc.
+type Filename struct {
+	// FString is the format string used to generate the image file name.
+	// It can be either a relative or an absolute path, as long as it
+	// is valid, ie. the directory exists.
+	// NOTE: the file extension determines the image format.
+	FString string `json:"f-string"`
+	// Fields are the names of camera.Image fields used as input to FString.
+	Fields []string `json:"fields"`
+}
+
+// NewFilename creates a new default Filename.
+func NewFilename() Filename {
+	return Filename{
+		FString: "img_%v_%v.jpg",
+		Fields: []string{
+			"Timestamp",
+			"ID",
+		},
+	}
+}
+
+// FIXME: reimplement so that 'reflect' is used only at setup
+func (f Filename) Values(img camera.Image) ([]any, error) {
+	val := reflect.ValueOf(img)
+	fieldVals := make([]any, 0, len(f.Fields))
+	for _, field := range f.Fields {
+		fld := val.FieldByName(field)
+		if !fld.IsValid() {
+			return nil, fmt.Errorf("invalid Filename field: %q", field)
+		}
+		switch fld.Kind() {
+		case reflect.Uint64:
+			fieldVals = append(fieldVals, strconv.FormatUint(fld.Uint(), 10))
+		case reflect.Int64:
+			fieldVals = append(fieldVals, strconv.FormatInt(fld.Int(), 10))
+		case reflect.Bool:
+			fieldVals = append(fieldVals, strconv.FormatBool(fld.Bool()))
+		case reflect.Struct:
+			if fld.Type() == reflect.TypeOf(time.Time{}) {
+				fieldVals = append(fieldVals, fld.Interface().(time.Time).Format("2006-01-02_15-04-05"))
+			} else {
+				return nil, fmt.Errorf("Filename field %q if of unsupported struct type: %q", field, fld.Type())
+			}
+		default:
+			return nil, fmt.Errorf("Filename field %q is of unsupported type: %q", field, fld.Kind())
+		}
+	}
+	return fieldVals, nil
+}
+
+// IsValid is an assertion that f is designates a valid file path, and
+// that f.Fields exist as fields of camera.Image.
+func (f Filename) Get(img camera.Image) (string, error) {
+	fieldVals, err := f.Values(img)
+	if err != nil {
+		return "", err
+	}
+	filename := fmt.Sprintf(f.FString, fieldVals...)
+	if _, err := os.Stat(path.Dir(filename)); err != nil {
+		return "", err
+	}
+	return filename, nil
+}
+
+type CamCmdStruct struct {
+	P  camera.Pylon `json:"pylon"`
+	F  Filename     `json:"filename"`
+	IP *ocr.ImageProcessor
+}
+
 func cam(cmd *cobra.Command, args []string) error {
 	runtime.LockOSThread()
 	defer runtime.UnlockOSThread()
@@ -35,64 +128,57 @@ func cam(cmd *cobra.Command, args []string) error {
 	if err != nil {
 		return err
 	}
-	// setup pylon
-	p := camera.NewPylon()
-	defer p.Finalize()
-	if err := json.Unmarshal(cfg, &p); err != nil {
+	ccs := CamCmdStruct{
+		P:  camera.NewPylon(),
+		F:  NewFilename(),
+		IP: ocr.NewImageProcessor(),
+	}
+	defer ccs.P.Finalize()
+	defer ccs.IP.Delete()
+	if err := json.Unmarshal(cfg, &ccs); err != nil {
 		return err
 	}
-	if err := p.Init(); err != nil {
+	if err := ccs.P.Init(); err != nil {
 		return err
 	}
-	// setup image processor
-	ip := ocr.NewImageProcessor()
-	defer ip.Delete()
-	// FIXME: this currently does nothing
-	if err := json.Unmarshal(cfg, &ip); err != nil {
-		return err
-	}
-	if err := ip.Init(); err != nil {
+	if err := ccs.IP.Init(); err != nil {
 		return err
 	}
 
 	log.Println("starting acquisition")
-	if err := p.C.StartAcquisition(); err != nil {
+	if err := ccs.P.C.StartAcquisition(); err != nil {
 		return err
 	}
-	defer p.C.StopAcquisition() // technically happens automatically
+	defer ccs.P.C.StopAcquisition() // technically happens automatically
 
-	// FIXME: should be set based on configuration
-	triggerTimeout, _ := time.ParseDuration("0.25s")
-	for p.C.IsAcquiring() {
-		log.Println("triggering...")
-		// FIXME: should trigger based on configuration
-		if err := p.C.WaitAndTrigger(triggerTimeout); err != nil {
-			// FIXME: should probably handle timeouts gracefully
-			return err
+	var acquired uint64
+	for ccs.P.C.IsAcquiring() && acquired < nReqAcquired {
+		// use the trigger if it's defined
+		if err := ccs.P.C.TryTrigger(); err != nil {
+			return err // FIXME: should probably handle timeouts gracefully
 		}
-
 		log.Println("acquiring...")
-		img, err := p.C.Acquire()
+		img, err := ccs.P.C.Acquire()
 		if err != nil {
 			return err
 		}
 		if img == nil {
 			continue
 		}
+		acquired++
 
-		log.Println("writing image with ID: ", img.ID)
 		// FIXME: output/processing should not block acquisition
+		log.Println("writing image with ID: ", img.ID)
 		if img.Monochrome {
-			err = ip.ReceiveMono8(img.Buffer(), img.Rows, img.Cols, img.Step)
+			err = ccs.IP.ReceiveMono8(img.Buffer(), img.Rows, img.Cols, img.Step)
 		} else {
-			err = ip.CopyBayerRGGB8(img.Buffer(), img.Rows, img.Cols, img.Step)
+			err = ccs.IP.CopyBayerRGGB8(img.Buffer(), img.Rows, img.Cols, img.Step)
 		}
-		// FIXME: should be runtime settable
-		filename := fmt.Sprintf("img_%v_%v.jpg",
-			img.Timestamp.Format("2006-01-02_15-04-05"),
-			strconv.FormatUint(img.ID, 10))
-		if err := ip.WriteImage(filename); err != nil {
-			log.Println("failed to write image")
+		if fname, err := ccs.F.Get(*img); err != nil {
+		} else {
+			if err := ccs.IP.WriteImage(fname); err != nil {
+				log.Println("failed to write image")
+			}
 		}
 		img.Delete()
 	}
