9cd7752b28680f5cd8ede8b3428049287794007b
Philipp Milovic <milovic.ph@gmail.com>
2024-08-23 13:23:52 +0200

    Hook up text detector to Go code

--- internal/ocr/ocr.cpp
+++ internal/ocr/ocr.cpp
@@ -23,6 +23,89 @@ void ResArr_Delete(void* r) {
 	}
 }
 
+
+bool DB_Init(DB d, const DBInit* in) {
+	if (!d || !in) {
+		return false;
+	}
+	d->modelPath = std::string(in->modelPath);
+	d->model = std::string(in->model);
+	d->binaryThreshold = in->binary;
+	d->polygonThreshold = in->polygon;
+	d->maxCandidates = in->maxCand;
+	d->unclipRatio = in->unclip;
+	d->useHardCodedMean = in->useHCMean;
+	return d->init();
+}
+
+DB DB_New() {
+	return new beholder::DBDetector {};
+}
+
+void Det_Clear(Det d) {
+	if (d) {
+		d->clear();
+	}
+}
+
+void Det_Delete(Det d) {
+	if (d) {
+		delete d;
+		d = nullptr;
+	}
+}
+
+ResArr* Det_Detect(Det d, const RawImage* img) {
+	if (!d || !img) {
+		return nullptr;
+	}
+	beholder::RawImage raw {
+		img->id,
+		img->rows,
+		img->cols,
+		img->pxTyp,
+		img->buf,
+		img->step
+	};
+	if (!d->detect(raw)) {
+		return nullptr;
+	}
+	const std::vector<beholder::Result>& results {d->getResults()};
+	Res* res {new Res[results.size()]};
+	for (auto i {0ul}; i < results.size(); ++i) {
+		const beholder::Result& raw {results[i]};
+		Res& ret {res[i]};
+
+		// copy the string
+		ret.text = new char[raw.text.size() + 1];
+		std::strncpy(ret.text, raw.text.c_str(), raw.text.size() + 1);
+
+		// copy the other stuff
+		ret.conf = raw.confidence;
+		ret.box.left = raw.box.left;
+		ret.box.top = raw.box.top;
+		ret.box.right = raw.box.right;
+		ret.box.bottom = raw.box.bottom;
+	}
+	return new ResArr{res, static_cast<size_t>(results.size())};
+}
+
+bool EAST_Init(EAST e, const EASTInit* in) {
+	if (!e || !in) {
+		return false;
+	}
+	e->modelPath = std::string(in->modelPath);
+	e->model = std::string(in->model);
+	e->confidenceThreshold = in->conf;
+	e->nmsThreshold = in->nms;
+	e->useHardCodedMean = in->useHCMean;
+	return e->init();
+}
+
+EAST EAST_New() {
+	return new beholder::EASTDetector {};
+}
+
 void Tess_Clear(Tess t) {
 	if (t) {
 		t->clear();
