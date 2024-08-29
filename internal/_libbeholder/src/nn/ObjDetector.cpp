2f0e3391864f8d8c1620b93e7df1758bb6c177c3
Philipp Milovic <milovic.ph@gmail.com>
2024-08-29 10:35:06 +0200

    Implement object detection

---
+++ internal/_libbeholder/src/nn/ObjDetector.cpp
@@ -0,0 +1,255 @@
+/*---------------------------------------------------------------------------*\
+
+	beholder - Copyright (C) 2024 P. Milovic
+
+-------------------------------------------------------------------------------
+License
+	See the LICENSE file for license information.
+
+\*---------------------------------------------------------------------------*/
+
+#include <filesystem>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <opencv2/core.hpp>
+#include <opencv2/core/fast_math.hpp>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/core/types.hpp>
+#include <opencv2/dnn/dnn.hpp>
+
+#include "ObjDetector.h"
+#include "Processor.h"
+#include "RawImage.h"
+#include "Rectangle.h"
+#include "Result.h"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace beholder
+{
+
+namespace detail
+{
+
+/*---------------------------------------------------------------------------*\
+                          Class Buffers Declaration
+\*---------------------------------------------------------------------------*/
+
+class Buffers
+{
+public:
+
+	// Public data
+
+	//- Temporaries used during ObjDetector::detect() and ObjDetector::extract()
+    cv::Mat blob;
+    std::vector<cv::Mat> outs;			// forward results
+	std::vector<cv::Rect> tBoxes;		// unfiltered blob boxes
+	std::vector<int> tClassIDs;			// unfiltered class IDs
+	std::vector<float> tConfidences;	// unfiltered confidences
+	std::vector<int> tNMSIDs;			// IDs used during NMS filtering
+
+	// Member functions
+
+		//- Clear buffers, but keep allocated memory.
+		void clear()
+		{
+			outs.clear();
+			tBoxes.clear();
+			tClassIDs.clear();
+			tConfidences.clear();
+			tNMSIDs.clear();
+		}
+};
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace detail
+
+// * * * * * * * * * * * * * * * Static Checks * * * * * * * * * * * * * * * //
+
+// DNN backend checks
+static_assert(static_cast<int>(NNBackend::BackendDefault) == static_cast<int>(cv::dnn::DNN_BACKEND_DEFAULT));
+static_assert(static_cast<int>(NNBackend::BackendHalide) == static_cast<int>(cv::dnn::DNN_BACKEND_HALIDE));
+static_assert(static_cast<int>(NNBackend::BackendOpenVINO) == static_cast<int>(cv::dnn::DNN_BACKEND_INFERENCE_ENGINE));
+static_assert(static_cast<int>(NNBackend::BackendOpenCV) == static_cast<int>(cv::dnn::DNN_BACKEND_OPENCV));
+static_assert(static_cast<int>(NNBackend::BackendVulkan) == static_cast<int>(cv::dnn::DNN_BACKEND_VKCOM));
+static_assert(static_cast<int>(NNBackend::BackendCUDA) == static_cast<int>(cv::dnn::DNN_BACKEND_CUDA));
+static_assert(static_cast<int>(NNBackend::BackendWebNN) == static_cast<int>(cv::dnn::DNN_BACKEND_WEBNN));
+static_assert(static_cast<int>(NNBackend::BackendTIMVX) == static_cast<int>(cv::dnn::DNN_BACKEND_TIMVX));
+static_assert(static_cast<int>(NNBackend::BackendCANN) == static_cast<int>(cv::dnn::DNN_BACKEND_CANN));
+
+// DNN target checks
+static_assert(static_cast<int>(NNTarget::TargetCPU) == 	static_cast<int>(cv::dnn::DNN_TARGET_CPU));
+static_assert(static_cast<int>(NNTarget::TargetOpenCL) == static_cast<int>(cv::dnn::DNN_TARGET_OPENCL));
+static_assert(static_cast<int>(NNTarget::TargetOpenCLfp16) == static_cast<int>(cv::dnn::DNN_TARGET_OPENCL_FP16));
+static_assert(static_cast<int>(NNTarget::TargetMyriad) == static_cast<int>(cv::dnn::DNN_TARGET_MYRIAD));
+static_assert(static_cast<int>(NNTarget::TargetVulkan) == static_cast<int>(cv::dnn::DNN_TARGET_VULKAN));
+static_assert(static_cast<int>(NNTarget::TargetFPGA) == static_cast<int>(cv::dnn::DNN_TARGET_FPGA));
+static_assert(static_cast<int>(NNTarget::TargetCUDA) == static_cast<int>(cv::dnn::DNN_TARGET_CUDA));
+static_assert(static_cast<int>(NNTarget::TargetCUDAfp16) == static_cast<int>(cv::dnn::DNN_TARGET_CUDA_FP16));
+static_assert(static_cast<int>(NNTarget::TargetHDDL) == static_cast<int>(cv::dnn::DNN_TARGET_HDDL));
+static_assert(static_cast<int>(NNTarget::TargetNPU) == static_cast<int>(cv::dnn::DNN_TARGET_NPU));
+static_assert(static_cast<int>(NNTarget::TargetCPUfp16) == static_cast<int>(cv::dnn::DNN_TARGET_CPU_FP16));
+
+// * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * * //
+
+void ObjDetector::extract()
+{
+	cv::transposeND(buf_->outs[0], {0, 2, 1}, buf_->outs[0]);
+
+	cv::Mat scores {};
+	for (auto out : buf_->outs)
+	{
+		out = out.reshape(1, out.size[1]); // [1, 8400, 85] -> [8400, 85]
+		for (auto i {0}; i < out.rows; ++i)
+		{
+			double conf;
+			cv::Point maxLoc {};
+			scores = out.row(i).colRange(4, out.cols);
+			cv::minMaxLoc(scores, 0, &conf, 0, &maxLoc);
+
+			if (conf < confidenceThreshold)
+			{
+			    continue;
+			}
+
+			// get bbox coords; [xCenter, yCenter, width, height]
+			float* det {out.ptr<float>(i)};
+			buf_->tBoxes.emplace_back
+			(
+				cvFloor(det[0] - 0.5 * det[2]),
+				cvFloor(det[1] - 0.5 * det[3]),
+				cvFloor(det[2]),
+				cvFloor(det[3])
+			);
+			buf_->tClassIDs.emplace_back(maxLoc.x);
+			buf_->tConfidences.emplace_back(static_cast<float>(conf));
+	    }
+	}
+
+	cv::dnn::NMSBoxes
+	(
+		buf_->tBoxes,
+		buf_->tConfidences,
+		confidenceThreshold,
+		nmsThreshold,
+		buf_->tNMSIDs
+	);
+}
+
+// * * * * * * * * * * * * Protected Member Functions  * * * * * * * * * * * //
+
+// * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * * //
+
+
+ObjDetector::ObjDetector()
+{}
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+ObjDetector::~ObjDetector()
+{}
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+void ObjDetector::clear()
+{
+	if (buf_)
+	{
+		buf_->clear();
+	}
+	res_.clear();
+}
+
+bool ObjDetector::detect(const RawImage& raw)
+{
+	clear();
+
+	if (net_->empty())
+	{
+		return false;
+	}
+	auto img {rawToMatPtr(raw)};
+	if (!img)
+	{
+		return false;
+	}
+
+	// preprocess
+	cv::dnn::blobFromImageWithParams(*img, buf_->blob, *params_);
+
+	// forward
+	net_->setInput(buf_->blob);
+	net_->forward(buf_->outs, net_->getUnconnectedOutLayersNames());
+
+	// extract and prep raw results for storage
+	extract();
+	params_->blobRectsToImageRects(buf_->tBoxes, buf_->tBoxes, img->size());
+
+	// store results
+	res_.reserve(buf_->tNMSIDs.size());
+	for (auto i {0ul}; i < buf_->tNMSIDs.size(); ++i)
+	{
+		auto id {buf_->tNMSIDs[i]};	// use NMS filtered IDs to select results
+		const cv::Rect& r {buf_->tBoxes[id]};
+		auto classID {buf_->tClassIDs[id]};
+		res_.emplace_back
+		(
+			Result
+			{
+				classes.size() > static_cast<size_t>(classID)
+			  ? classes[classID] : std::to_string(classID),
+				Rectangle {r.x, r.y, r.x + r.width, r.y + r.height},
+				static_cast<double>(buf_->tConfidences[id])
+			}
+		);
+	}
+	return !res_.empty();
+}
+
+const std::vector<Result>& ObjDetector::getResults() const
+{
+	return res_;
+}
+
+bool ObjDetector::init()
+{
+	// XXX: no checks, we assume that it's been checked and is correct; yolo
+	// must be an '.onnx' file though
+	std::filesystem::path modelFile
+	{
+		std::filesystem::path{modelPath} / model
+	};
+	// TODO: we should supply a buffer and read from memory
+	net_.reset(new cv::dnn::Net {cv::dnn::readNetFromONNX(modelFile.string())});
+    net_->setPreferableBackend(backend);
+    net_->setPreferableTarget(target);
+	params_.reset
+	(
+		new cv::dnn::Image2BlobParams
+		{
+			cv::Scalar::all(scale),
+			cv::Size {size, size},
+			cv::Scalar::all(mean),
+			swapRB,
+			CV_32F,
+			cv::dnn::DNN_LAYOUT_NCHW,
+			cv::dnn::DNN_PMODE_LETTERBOX,
+			cv::Scalar::all(padValue)
+		}
+	);
+	buf_.reset(new detail::Buffers {});
+
+	return !net_->empty();
+}
+
+// * * * * * * * * * * * * * * Helper Functions  * * * * * * * * * * * * * * //
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace beholder
+
+// ************************************************************************* //
---
+++ internal/_libbeholder/src/nn/ObjDetector.h
@@ -0,0 +1,224 @@
+/*---------------------------------------------------------------------------*\
+
+	beholder - Copyright (C) 2024 P. Milovic
+
+-------------------------------------------------------------------------------
+License
+	See the LICENSE file for license information.
+
+Description
+	A DNN-based object detector wrapper.
+
+	WARNING: models operate on images sized as multiples of 32px.
+
+SourceFiles
+	ObjDetector.cpp
+
+\*---------------------------------------------------------------------------*/
+
+#ifndef BEHOLDER_OBJ_DETECTOR_H
+#define BEHOLDER_OBJ_DETECTOR_H
+
+#ifndef CV__DNN_INLINE_NS
+#define CV__DNN_INLINE_NS_BEGIN
+#define CV__DNN_INLINE_NS_END
+#endif
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "RawImage.h"
+#include "Result.h"
+
+// * * * * * * * * * * * * * Forward Declarations  * * * * * * * * * * * * * //
+
+namespace cv
+{
+	class Mat;
+
+	namespace dnn
+	{
+		CV__DNN_INLINE_NS_BEGIN
+			class Net;
+			class Image2BlobParams;
+		CV__DNN_INLINE_NS_END
+	}
+}
+
+namespace beholder
+{
+	namespace detail
+	{
+		// Buffers is a helper class which has buffers for various temporary
+		// values created during object detection.
+		//
+		// It is forward declared to avoid having to include OpenCV headers
+		// here.
+		class Buffers;
+	}
+}
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace beholder
+{
+
+// can't do enum class because Go will complain.
+enum NNBackend
+{
+	BackendDefault = 0,
+	BackendHalide,
+	BackendOpenVINO,
+	BackendOpenCV,
+	BackendVulkan,
+	BackendCUDA,
+	BackendWebNN,		// Microsoft WebNN
+	BackendTIMVX,		// VeriSilicon TIM-VX
+	BackendCANN,		// Huawei CANN backend
+};
+
+// can't do enum class because Go will complain
+enum NNTarget
+{
+	TargetCPU = 0,
+	TargetOpenCL,
+	TargetOpenCLfp16,
+	TargetMyriad,		// Xailient AI Myriad 2/X
+	TargetVulkan,
+	TargetFPGA,
+	TargetCUDA,
+	TargetCUDAfp16,
+	TargetHDDL,		// OpenVINO HDDL
+	TargetNPU,
+	TargetCPUfp16,	// WARNING: ARM only
+};
+
+
+/*---------------------------------------------------------------------------*\
+                      Class ObjDetector Declaration
+\*---------------------------------------------------------------------------*/
+
+class ObjDetector
+{
+private:
+
+	// Private data
+
+		//- A pointer to an OpenCV DNN
+		//	NOTE: a unique_ptr would be nicer, but cgo keeps complaining
+		std::unique_ptr<cv::dnn::Net> net_ {nullptr};
+
+		//- Processing parameters of image to blob.
+		//	NOTE: a unique_ptr would be nicer, but cgo keeps complaining
+		std::unique_ptr<cv::dnn::Image2BlobParams> params_ {nullptr};
+
+		//- Buffers for temporary values used during detection
+		//	WARNING: Go might complain because of the unique_ptr
+		std::unique_ptr<detail::Buffers> buf_ {nullptr};
+
+		//- detection results
+		std::vector<Result> res_;
+
+	// Private member functions
+
+		//- Extract inference results
+		//	TODO: we would like to time this externally, somehow
+		void extract();
+
+public:
+
+	// Public data
+
+		//- Directory path of the model (weights) file.
+		std::string modelPath;
+		//- Model (weights) name.
+		//
+		//	NOTE: the model should be in ONNX format.
+		std::string model;
+
+		//- A list of object clases that the loaded model supports.
+		std::vector<std::string> classes;
+
+		//- The network computation backend, see:
+		//	https://docs.opencv.org/4.10.0/d6/d0f/group__dnn.html#ga186f7d9bfacac8b0ff2e26e2eab02625
+		//
+		//	NOTE: OpenCV needs to be compiled with additional backend support,
+		//	eg. CUDA/CUDNN, OpenVINO...
+		int backend {NNBackend::BackendDefault};
+		//- The network target device for computations, see:
+		//	https://docs.opencv.org/4.10.0/d6/d0f/group__dnn.html#ga709af7692ba29788182cf573531b0ff5
+		int target {NNTarget::TargetCPU};
+
+		//- Image size in px.
+		//	The image will be resized and padded so that the largest image
+		//	dimension is equal to size, and the original aspect ratio is
+		//	preserved.
+		int size {640};
+		//- Multiplier for image pixel values.
+		//	Set based on which model is in use.
+		double scale {1.0/255.0};
+		//- Confidence threshold used for text box filtering.
+		float confidenceThreshold {0.5};
+		//- Non-maximum suppression threshold.
+		float nmsThreshold {0.4};
+		//- Normalization constant.
+		//	This value is subtracted from each pixel value of the current image.
+        double mean {0.0};
+		//- Swap red and green channels
+        bool swapRB {true};
+		//- The pixel value used to pad the image, if padding is active.
+        float padValue {0.0};	// or 114.0?
+
+
+	// Constructors
+
+		//- Default constructor
+		ObjDetector();
+
+		//- Disable copy constructor
+		ObjDetector(const ObjDetector&) = default;
+
+		//- Disable move constructor
+		ObjDetector(ObjDetector&&) = default;
+
+
+	//- Destructor
+	~ObjDetector();
+
+	// Member functions
+
+		//- Clear detection results
+		void clear();
+
+		//- Run text detection and store the results
+		bool detect(const RawImage& raw);
+
+		//- Get a const reference to the detection results
+		const std::vector<Result>& getResults() const;
+
+		//- Initialize the object detector.
+		bool init();
+
+
+	// Member operators
+
+		//- Disable copy assignment
+		ObjDetector& operator=(const ObjDetector&) = default;
+
+		//- Disable move assignment
+		ObjDetector& operator=(ObjDetector&&) = default;
+
+};
+
+// * * * * * * * * * * * * * * Helper Functions  * * * * * * * * * * * * * * //
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace beholder
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+#endif
+
+// ************************************************************************* //
