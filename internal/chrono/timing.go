a49034d7e291e9a73f14250a535c45254789347b
Philipp Milovic <milovic.ph@gmail.com>
2024-09-21 04:25:00 +0200

    Add comments

--- internal/chrono/timing.go
+++ internal/chrono/timing.go
@@ -11,14 +11,23 @@ type Timing struct {
 	Value time.Duration
 }
 
+// Timings is a convenience type for easy handling of a set of timings.
 type Timings []Timing
 
+// Clear sets the length of ts to 0.
+func (ts *Timings) Clear() {
+	*ts = (*ts)[:0]
+}
+
+// Reset sets the duration of each [Timing] in ts to 0.
 func (ts *Timings) Reset() {
 	for i := range *ts {
 		(*ts)[i].Value = time.Duration(0)
 	}
 }
 
+// Set sets the duration value of the [Timing] name to val. If name is
+// not present in ts, it is appended to ts with the value val.
 func (ts *Timings) Set(name string, val time.Duration) {
 	for i := range *ts {
 		if (*ts)[i].Name == name {
@@ -29,6 +38,7 @@ func (ts *Timings) Set(name string, val time.Duration) {
 	*ts = append(*ts, Timing{Name: name, Value: val})
 }
 
+// String returns the formatted string representation of ts.
 func (ts Timings) String() string {
 	var b strings.Builder
 	for i := range ts {
