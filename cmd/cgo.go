1256797aac1dd38adcec98531a1995487dc06e1f
Philipp Milovic <milovic.ph@gmail.com>
2025-01-28 16:34:12 +0100

    Handle release/debug configs through 'dev'
    - handle variable cgo compiler/linker flags through environment
      variables
    - 'dev' correctly appends/prepends additional flags to environment
      variables needed to run subsequent scripts/commands
    - remove stale cgo flags from Go modules
        - handled through 'dev'

--- cmd/cgo.go
+++ cmd/cgo.go
@@ -11,14 +11,10 @@ package cmd
 //	- internal/camera/cgo.go
 //	- internal/imgproc/cgo.go
 //	- internal/neural/cgo.go
-//
-// TODO: include/link directories should probably get loaded through
-// environment variables.
 
 /*
 #cgo CXXFLAGS: -std=c++20
 #cgo pkg-config: beholder_camera
-#cgo CPPFLAGS: -I${SRCDIR}/../../_c-api/build/staging/include
-#cgo linux LDFLAGS: -L${SRCDIR}/../../_c-api/build/staging/include/lib -lbeholder -ltesseract -lleptonica -lopencv_videoio -lopencv_video -lopencv_imgcodecs -lopencv_dnn -lopencv_photo -lopencv_imgproc -lopencv_core -littnotify -lIlmImf -lippiw -lippicv -lade -llibjpeg-turbo -llibopenjp2 -llibpng -llibprotobuf -llibtiff -llibwebp -lzlib
+#cgo LDFLAGS:-lbeholder -ltesseract -lleptonica -lopencv_videoio -lopencv_video -lopencv_imgcodecs -lopencv_dnn -lopencv_photo -lopencv_imgproc -lopencv_core -littnotify -lIlmImf -lippiw -lippicv -lade -llibjpeg-turbo -llibopenjp2 -llibpng -llibprotobuf -llibtiff -llibwebp -lzlib
 */
 import "C"
--- internal/neural/cgo.go
+++ internal/neural/cgo.go
@@ -4,14 +4,10 @@
 
 package neural
 
-// Changes here should be mirrored in internal/imgproc/cgo.go.
-//
-// TODO: include/link directories should probably get loaded through
-// environment variables.
+// NOTE: Changes here should be mirrored in internal/imgproc/cgo.go.
 
 /*
 #cgo CXXFLAGS: -std=c++20
-#cgo CPPFLAGS: -I${SRCDIR}/../../_c-api/build/staging/include
-#cgo linux LDFLAGS: -L${SRCDIR}/../../_c-api/build/staging/include/lib -lbeholder -ltesseract -lleptonica -lopencv_videoio -lopencv_video -lopencv_imgcodecs -lopencv_dnn -lopencv_photo -lopencv_imgproc -lopencv_core -littnotify -lIlmImf -lippiw -lippicv -lade -llibjpeg-turbo -llibopenjp2 -llibpng -llibprotobuf -llibtiff -llibwebp -lzlib
+#cgo LDFLAGS:-lbeholder -ltesseract -lleptonica -lopencv_videoio -lopencv_video -lopencv_imgcodecs -lopencv_dnn -lopencv_photo -lopencv_imgproc -lopencv_core -littnotify -lIlmImf -lippiw -lippicv -lade -llibjpeg-turbo -llibopenjp2 -llibpng -llibprotobuf -llibtiff -llibwebp -lzlib
 */
 import "C"
